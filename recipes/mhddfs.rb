0ca37bebd55d77fbdb92628de1c2312d66d35bdd
haga <haganenorekenjutsushi@gmail.com>
2015-06-14 15:34:35 +0100

    Added a per-disk reservation of 10GB to allow snapraid content files to live on the disks

--- recipes/mhddfs.rb
+++ recipes/mhddfs.rb
@@ -12,7 +12,7 @@ source_disks_semicolon = node['torrentbox']['mhddfs']['source_disks'].join(';')
 
 execute 'mount mhddfs virtual directory' do
   command <<-EOH
-  sudo mhddfs -s #{source_disks_comma} #{node['torrentbox']['mhddfs']['mount_point']} -o allow_other,nonempty
+  sudo mhddfs -s #{source_disks_comma} #{node['torrentbox']['mhddfs']['mount_point']} -o allow_other,nonempty,mlimit=10G
   EOH
   # Don't try to mount the disks to the virtual directory if they're already mounted
   not_if "grep \"#{source_disks_semicolon} #{node['torrentbox']['mhddfs']['mount_point']}\" /etc/mtab"
